c4262b774015a236e405403271aaacc759c54d7c
Livia Banyai <livia5banyai@gmail.com>
2026-01-28 22:48:37 +0000

    editing signup.html to deal with errors more effectively

--- food-waste-rescue/main/forms.py
+++ food-waste-rescue/main/forms.py
@@ -13,12 +13,6 @@ class GenericSignupForm(UserCreationForm):
         fields = ['user_type', 'username', 'email','password1', 'password2'] 
  
 class SellerExtraForm(forms.ModelForm):  
-    #location = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"placeholder": "Location"}))
-    #opening_time = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"placeholder": "Opening Time"}))
-    #closing_time = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"placeholder": "Closing Time"}))
-    #telephone_number = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"placeholder": "Telephone Number"}))
-    #website_URL = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"placeholder": "Website URL"}))
- 
     class Meta:  
         model = Seller
         fields = ['location','opening_time','closing_time','telephone_number','website_url']  
