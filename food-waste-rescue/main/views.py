24a078b85baffee542121203c02caf484edc9a2a
Livia Banyai <livia5banyai@gmail.com>
2026-01-29 17:32:05 +0000

    setup signup redirects

--- food-waste-rescue/main/views.py
+++ food-waste-rescue/main/views.py
@@ -13,24 +13,27 @@ def testView(request):
 
 ########### register here ##################################### 
 def registerUser(request):
-    if request.method == 'POST':
-        form = GenericSignupForm(request.POST)
-        if form.is_valid():
-            user = form.save() #returns custom user instance
+    if request.user.is_authenticated:
+        return redirect("/") #TODO: Change to home page
+    else:    
+        if request.method == 'POST':
+            form = GenericSignupForm(request.POST)
+            if form.is_valid():
+                user = form.save() #returns custom user instance
 
-            if user.user_type == "seller":
-                return redirect("seller-extra", user_id=user.id)
+                if user.user_type == "seller":
+                    return redirect("seller-extra", user_id=user.id)
+                else:
+                    #user = User.objects.create_user(form.cleaned_data.get("username"),form.cleaned_data("email"),form.cleaned_data("password1"),form.cleaned_data["user_type"],form.cleaned_data("password2")),
+                    messages.success(request, f'Your account has been created! You are now able to log in')
+                    return redirect("login")
             else:
-                #user = User.objects.create_user(form.cleaned_data.get("username"),form.cleaned_data("email"),form.cleaned_data("password1"),form.cleaned_data["user_type"],form.cleaned_data("password2")),
-                messages.success(request, f'Your account has been created! You are now able to log in')
-                return redirect("login")
+                messages.info(request, f'Check your details.')
+                form = GenericSignupForm()
+                return render(request, 'registration/signup.html', {'form': form, 'title':'register here'})
         else:
-            messages.info(request, f'Check your details.')
             form = GenericSignupForm()
             return render(request, 'registration/signup.html', {'form': form, 'title':'register here'})
-    else:
-        form = GenericSignupForm()
-        return render(request, 'registration/signup.html', {'form': form, 'title':'register here'})
             
 
 
