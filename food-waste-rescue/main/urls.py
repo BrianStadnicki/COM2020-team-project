1b448300322509962ad4f48dbff86423dbaf26c0
Brian Stadnicki <stadnickibrian@gmail.com>
2026-02-01 15:08:11 +0000

    setup proper views and urls

--- food-waste-rescue/main/urls.py
+++ food-waste-rescue/main/urls.py
@@ -2,40 +2,18 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    
-    # main paths
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", views.home_view, name="home_view_url"),
     path("test/", views.test_view, name="test_view_url"),
-    path("bundles/", views.bundles_view, name="bundles_view_url"),
+    path("bundles", views.bundles_view, name="bundles_view_url"),
+    path("bundle/<int:id>", views.bundle_view, name="bundle_view_url"),
+    path("bundle/new", views.bundle_new_view, name="bundle_new_view_url"),
+    path("bundle/confirm", views.bundle_confirm_view, name="bundle_confirm_view_url"),
     path("reservations/", views.reservations_view, name="reservations_view_url"),
-    
-    #seller paths
-    path("seller/", views.seller_view, name="seller_view_url"),
-    path("seller/profile/", views.seller_profile_view, name="seller_profile_view_url"),
-    path("seller/reports/", views.seller_reports_view, name ="seller_reports_view_url"),
-    path("seller/response/", views.seller_response_view, name="seller_response_view_url"),
-    path("seller/accessbility/", views.seller_accessibility_view, name="seller_accessibility_view_url"),
-    path("seller/bundles/", views.seller_bundles_view, name="seller_bundles_view_url"),
-    path("seller/bundles/create/", views.seller_create_bundles_view, name="seller_create_bundles_view_url"),
-    path("seller/bundles/finalise", views.seller_finalise_bundles_view, name="seller_finalise_bundles_view_url"),
-    path("seller/reservations/", views.seller_reservations_view, name="seller_reservations_view_url"),
-    path("seller/analytics/", views.seller_analytics_view, name="seller_analytics_view_url"),
-    path("seller/activity/", views.seller_activity_view, name="seller_activity_view_url"),
-     
-    #consumer paths
-    path("consumer/", views.consumer_view, name="consumer_view_url"),
-    path("consumer/profile/", views.consumer_profile_view, name="consumer_profile_view_url"),
-    path("consumer/accessibility/", views.consumer_accessibility_view, name="consumer_accessibility_view_url"),
-    path("consumer/impact/", views.consumer_impact_view, name="consumer_impact_view_url"),
-    path("consumer/details/", views.consumer_details_view, name="consumer_details_view_url"),
-    path("consumer/bundles/", views.consumer_bundles_view, name="consumer_bundles_view_url"),
-    path("consumer/reservations/", views.consumer_reservations_view, name="consumer_reservations_view_url"),
-    path("consumer/reports/", views.consumer_reports_view, name="consumer_reports_view_url"),
-   
-   #developer paths
-   path("developer/", views.developer_view, name = "developer_view_url"),
-   path("developer/reports/", views.developer_reports_view, name="developer_reports_url"),
-   path("developer/company/", views.developer_company_view, name="developer_company_url"),
-   path("developer/developer_company", views.developer_developer_company_view, name="developer_developer_company_url")
+    path("reservations/<int:id>", views.reservation_view, name="reservation_view_url"),
+    path("analytics/", views.analytics_view, name="analytics_view_url"),
+    path("reports/", views.reports_view, name="reports_view_url"),
+    path("report/<int:id>", views.report_view, name="report_view_url"),
+    path("report/new", views.report_new_view, name="report_new_view_url"),
+    path("impact", views.impact_view, name="impact_view_url"),
+    path("accessibility", views.accessibility_view, name="accessibility_view_url"),
 ]
--- food-waste-rescue/main/views.py
+++ food-waste-rescue/main/views.py
@@ -3,145 +3,87 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django import forms
-# from .forms import ...
 
 def test_view(request):
     return render(request, "main/test.html")
 
-def home_view(request):
-    return render(request, "main/home.html")
-
+"""
+Consumer: Show all bundles, search by location and pick up time, pagination
+Seller: Show own bundles, pagination
+"""
 def bundles_view(request):
     return render(request, "main/bundles.html")
 
+"""
+Consumer: Show bundle, make new reservation or view own reservation details
+Seller: show/edit/delete bundle, change reservation status?
+"""
+def bundle_view(request, id):
+    return render(request, "main/bundle.html")
+
+"""
+Seller: create new bundle
+"""
+def bundle_new_view(request):
+    return render(request, "main/bundle_new.html")
+
+"""
+Seller: See analytics, actually create
+"""
+def bundle_confirm_view(request):
+    return render(request, "main/bundle_confirm.html")
+
+"""
+Consumer: Show own reservations with bundle details
+Seller: Show upcoming reservations with bundle details, edit status, search/verify code
+"""
 def reservations_view(request):
     return render(request, "main/reservations.html")
 
-# Seller Views
-def seller_view(request):
-    return render(request, "main/seller.html")
+"""
+Consumer: Show/delete own reservation with bundle details
+Seller: Show/edit own reservation with bundle details
+"""
+def reservation_view(request, id):
+    return render(request, "main/reservation.html")
 
-def seller_profile_view(request):
-    return render(request, "main/seller_profile.html") 
+"""
+Seller: Show analytics
+"""
+def analytics_view(request):
+    return render(request, "main/analytics.html")
 
-def seller_reports_view(request):
-    return render(request, "main/seller_reports.html")
+"""
+Consumer: Show own reports
+Seller: Show own reports
+"""
+def reports_view(request):
+    return render(request, "main/reports.html")
 
-def seller_response_view(request):
-    
-    # Temporary form until created forms can be imported 
-    form_class = forms.Form
-    form = form_class()
-    # 
-    
-    if request.method == "POST":   
-        form = form_class(request.POST)  
-        if form.is_valid():
-            # form.save() and/or something else
-            pass
-    else: 
-        form = form_class()
-    return render(request, "main/seller_response.html") # , {"form":form}
+"""
+Consumer: Show/add/close own report
+Seller: Show/add/close own report
+"""
+def report_view(request, id):
+    return render(request, "main/report.html")
 
-def seller_accessibility_view(request):
-    return render(request, "main/seller_accessibility.html")
+"""
+Consumer: Create new report
+Seller: Create new report
+"""
+def report_new_view(request):
+    return render(request, "main/report_new.html")
 
-def seller_bundles_view(request):
-    return render(request, "main/seller_bundles.html")
+"""
+Consumer: View impact
+Seller: View impact
+"""
+def impact_view(request):
+    return render(request, "main/impact.html")
 
-def seller_create_bundles_view(request):
-    
-    # Temporary form until created forms can be imported 
-    form_class = forms.Form
-    form = form_class()
-    # 
-    
-    if request.method == "POST":   
-        form = form_class(request.POST)  
-        if form.is_valid():
-            # form.save() and/or something else
-            pass
-    else: 
-        form = form_class()
-    
-    return render(request, "main/seller_create_bundles.html") #, {"form":form}
-
-def seller_finalise_bundles_view(request):
-
-    # Temporary form until created forms can be imported 
-    form_class = forms.Form
-    form = form_class()
-    # 
-    
-    if request.method == "POST":   
-        form = form_class(request.POST)  
-        if form.is_valid():
-            # form.save() and/or something else
-            pass
-    else: 
-        form = form_class()
-
-    return render(request, "main/seller_finalise_bundles.html") #, {"form":form}
-
-def seller_reservations_view(request):
-    return render(request, "main/seller_reservations.html")
-
-def seller_analytics_view(request):
-    return render(request, "main/seller_analytics.html")
-
-def seller_activity_view(request):
-    return render(request, "main/seller_activity.html")
-
-# Consumer Views
-
-def consumer_view(request):
-    return render(request, "main/consumer.html")
-
-def consumer_profile_view(request):
-    return render(request, "main/consumer_profile.html") 
-
-def consumer_accessibility_view(request):
-    return render(request, "main/consumer_accessibility.html")
-
-def consumer_impact_view(request):
-    return render(request, "main/consumer_impact.html")
-
-def consumer_details_view(request):
-    
-    # Temporary form until created forms can be imported 
-    form_class = forms.Form
-    form = form_class()
-    # 
-    
-    if request.method == "POST":   
-        form = form_class(request.POST)  
-        if form.is_valid():
-            # form.save() and/or something else
-            pass
-    else: 
-        form = form_class()    
-    
-    return render(request, "main/consumer_details.html") #, {"form":form}
-
-def consumer_bundles_view(request):
-    return render(request, "main/consumer_bundles.html")
-
-def consumer_reservations_view(request):
-    return render(request, "main/consumer_reservations.html")
-
-def consumer_reports_view(request):
-    return render(request, "main/consumer_reports.html")
-
-# Developer Views
-
-def developer_view(request):
-    return render(request, "main/developer.html")
-
-def developer_reports_view(request):
-    return render(request, "main/developer_reports.html")
-
-def developer_company_view(request):
-    return render(request, "main/developer_company.html")
-
-def developer_developer_company_view(request):
-    return render(request, "main/developer_developer_company.html")
+"""
+Consumer: View/Change accessibility settings
+Seller: View/Change accessibility settings
+"""
+def accessibility_view(request):
+    return render(request, "main/accessibility.html")
