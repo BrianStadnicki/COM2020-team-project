f8adea805f15ba4cab733442cc05a174e078a6ff
Livia Banyai <livia5banyai@gmail.com>
2026-02-03 21:27:02 +0000

    tests for report_view(id)

--- food-waste-rescue/main/tests/test_private_pages.py
+++ food-waste-rescue/main/tests/test_private_pages.py
@@ -256,5 +256,27 @@ class testSellerAndConsumerPages(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
+    # ---------------------------------------------------------------------------
+
+    #Tests for report_view(id)
+    def test_report_view_redirects_for_anonymous(self):
+        """Anonymous users should get 302 Redirect and be redirected to login"""
+        url = reverse("report_view_url", args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/accounts/login", response.url)
     
+    def test_report_view_allows_consumer(self):
+        """Consumers should get 200 OK"""
+        self.client.login(username="consumer2", password="consumerpass2")
+        url = reverse("report_view_url", args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_report_view_allows_seller(self):
+        """Sellers shoud get 200 OK"""
+        self.client.login(username="seller2", password="sellerpass2")
+        url = reverse("report_view_url", args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
