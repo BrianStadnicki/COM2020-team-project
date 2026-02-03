425730a995c774b5cdd04528c7c7a7856a61a057
Livia Banyai <livia5banyai@gmail.com>
2026-02-03 21:10:50 +0000

    tests for reports_view

--- food-waste-rescue/main/tests/test_private_pages.py
+++ food-waste-rescue/main/tests/test_private_pages.py
@@ -215,7 +215,7 @@ class testSellerAndConsumerPages(TestCase):
         url = reverse("bundle_view_url", args=[1])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        self.assertIn("accounts/login", response.url)
+        self.assertIn("/accounts/login", response.url)
 
     def test_bundle_view_allows_consumer(self):
         """Consumers should get 200 OK"""
@@ -228,6 +228,31 @@ class testSellerAndConsumerPages(TestCase):
         """Sellers should get 200 OK"""
         self.client.login(username="seller2", password="sellerpass2")
         url = reverse("bundle_view_url", args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    #-----------------------------------------------------------------------------
+
+    #Tests for reports_view
+
+    def test_reports_view_redirects_for_anonymous(self):
+        """Anonymous users should get 302 Redirect and be redirected to login"""
+        url = reverse("reports_view_url")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn("/accounts/login", response.url)
+    
+    def test_reports_view_allows_consumer(self):
+        """Consumers should get 200 OK"""
+        self.client.login(username="consumer2", password="consumerpass2")
+        url = reverse("reports_view_url")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_reports_view_allows_seller(self):
+        """Sellers shoud get 200 OK"""
+        self.client.login(username="seller2", password="sellerpass2")
+        url = reverse("reports_view_url")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
