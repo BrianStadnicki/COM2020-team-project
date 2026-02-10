ad9d3233cebb0f94d12db81af4900445757e0cf0
Brian Stadnicki <stadnickibrian@gmail.com>
2026-02-10 19:25:58 +0000

    fixed up forecast calc

--- food-waste-rescue/main/forecast_calc.py
+++ food-waste-rescue/main/forecast_calc.py
@@ -1,38 +1,33 @@
 # Contains for forecast
 from .models import Bundle_posting, Seller, Reservation
-from django.db.models import Q
+from django.db.models import Sum, Count, F
 from django.db import models
 from django.utils import timezone
-import datetime
+from datetime import time, datetime, timedelta
+
 
 """ Fetch the Sellers Bundle Reservation from the last 4 weeks
     and calculate average percentage number of reservations."""
-def avePerRes( Sid ):
-    return 2
-    date = models.DateTimeField(default=timezone.now,blank=True)
-    seller = Seller.objects.get(id=Sid)
-    # Epoch number for 4 weeks earlier
-    four_weeks = 2419200
-    # Find bundles and reservations in time frame
-    bundles = seller.bundle_posting_set.all().filter(creation_time = date - four_weeks)
-    reserved = seller.reservation_set.all().filter(creation_time = date - four_weeks)
-    num_of_bundles = len(bundles)
-    num_of_reservations = len(reserved)
+def avePerRes(seller_id ):
+    recent_postings = Bundle_posting.objects.filter(seller_id=seller_id)
+    recent_postings = recent_postings.filter(creation_time__gte=timezone.now() - timedelta(weeks=4))
+    recent_postings_quantity_count = recent_postings.aggregate(Sum("quantity"))["quantity__sum"]
+
+    recent_reservations_count = Reservation.objects.filter(posting__seller_id=seller_id).count()
 
     # return percentage calculation
-    return (num_of_reservations/num_of_bundles)*100
+    return (recent_reservations_count/recent_postings_quantity_count)
 
 """ Fetch the Sellers Bundle Reservations from the last 4 weeks
     and calculate average percentage number of no-shows. """
-def avePerNoshow(Sid):
+def avePerNoshow(seller_id):
+    recent_reservations = Reservation.objects.filter(posting__seller_id=seller_id)
+    recent_reservations = recent_reservations.filter(time_stamp__gte=timezone.now() - timedelta(weeks=4))
+    recent_reservations_count = recent_reservations.count()
 
-    return 2
-    date = models.DateTimeField(default=timezone.now,blank=True)
-    seller = Seller.objects.get(id=Sid)
-    four_weeks = 2419200
-    reservations = seller.reservation_set.all().filter(creation_time = date - four_weeks)
-    no_shows = seller.reservation_set.all().filter(creation_time = date - four_weeks).filter(status = "N")
-    num_of_reservations = len(reservations)
-    num_of_no_shows = len(no_shows)
+    no_shows = 0;
+    for reservation in recent_reservations.all():
+        if reservation.status == "N":
+            no_shows += 1
 
-    return (num_of_no_shows/num_of_reservations)*100
+    return (no_shows/recent_reservations_count)
