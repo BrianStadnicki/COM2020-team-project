a59c2659b0bae7946d0f252ca7cc0ee576a12703
Brian Stadnicki <stadnickibrian@gmail.com>
2026-02-14 15:43:59 +0000

    run format

--- food-waste-rescue/main/management/commands/export.py
+++ food-waste-rescue/main/management/commands/export.py
@@ -7,6 +7,7 @@ import datetime
 
 logger = logging.getLogger(__name__)
 
+
 def tz_info_convert(attr):
     """Convert tz_info to None"""
     if isinstance(attr, datetime.datetime):
@@ -21,18 +22,18 @@ def tz_info_convert(attr):
 
     return attr
 
+
 class Command(BaseCommand):
-    help="Export database to an xlsx file."
+    help = "Export database to an xlsx file."
 
     def handle(self, *args, **options):
-        
         wb = Workbook()
         wb.remove(wb.active)
         models = [Consumer, Seller, Bundle_posting, Reservation, IssueReport]
-        
+
         for model in models:
             ws = wb.create_sheet(model.__name__)
-        
+
             fields = list(model._meta.fields)
             columns = []
             for field in fields:
@@ -40,21 +41,19 @@ class Command(BaseCommand):
                     columns.append(f"{field.name}_id")
                 else:
                     columns.append(field.name)
-            
+
             ws.append(columns)
-            
+
             for object in model.objects.all():
                 attributes = []
                 for field in fields:
                     if field.is_relation:
-                        attributes.append(tz_info_convert(getattr(object, field.attname)))
+                        attributes.append(
+                            tz_info_convert(getattr(object, field.attname))
+                        )
                     else:
                         attributes.append(tz_info_convert(getattr(object, field.name)))
                 ws.append(attributes)
-                
+
         wb.save("food_waste_rescue.xlsx")
         self.stdout.write("Saved food_waste_rescue.xlsx")
-        
-
-    
-    
