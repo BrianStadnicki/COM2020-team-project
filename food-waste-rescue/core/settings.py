841610aa118b71b82ad48ad88ebb3f8af3ac6537
Brian Stadnicki <stadnickibrian@gmail.com>
2026-03-08 22:33:12 +0000

    add production url

--- food-waste-rescue/core/settings.py
+++ food-waste-rescue/core/settings.py
@@ -31,7 +31,7 @@ else:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "bs666.eu.pythonanywhere.com"]
 
 
 # Application definition
