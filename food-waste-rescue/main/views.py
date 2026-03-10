9e78287534a3b74c30ad78dcbbcdb64083611353
Ben Hambleton <ben.t.hambleton@gmail.com>
2026-03-10 17:50:34 +0000

    Added filtering by inactive. Removed status from bundles view, and added available status on bundle view.

--- food-waste-rescue/main/views.py
+++ food-waste-rescue/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q, Count, F
 from .forms import (
     ReservationForm,
     SellerExtraForm,
@@ -67,6 +67,8 @@ def bundles_view(request):
     
     selected_allergens = request.GET.getlist("excluded-allergens")
     selected_wheelchair = request.GET.get("wheelchair")
+    selected_inactive = request.GET.get("show-inactive")
+    selected_expired = request.GET.get("show-expired")
 
     if selected_category_id != "":
         selected_category = Bundle_posting_category.objects.get(id=selected_category_id)
@@ -74,6 +76,20 @@ def bundles_view(request):
 
     if selected_wheelchair:
         posts = posts.filter(seller__wheelchair=True)
+
+    hidden_posts = posts.none()
+
+    annotated_posts = posts.annotate(collected_count=Count("reservation", filter=Q(reservation__is_collected=True)))
+
+    if not selected_inactive:
+        hidden_posts |= annotated_posts.filter(collected_count=F("quantity"))
+
+    if not selected_expired:
+        hidden_posts |= annotated_posts.exclude(collected_count=F("quantity")
+        ).exclude(creation_time__date=datetime.datetime.today().date(),
+            pickup_window_end__gt=datetime.datetime.today().time())
+
+    posts = posts.exclude(id__in=hidden_posts.values("id"))
 
     if selected_allergens:
         q = Q()
@@ -95,7 +111,9 @@ def bundles_view(request):
             "selected_category": selected_category,
             "selected_allergens": selected_allergens,
             "selected_location": location,
-            "selected_wheelchair": selected_wheelchair
+            "selected_wheelchair": selected_wheelchair,
+            "selected_expired": selected_expired,
+            "selected_inactive": selected_inactive
         },
     )
 
